02961d4bb5faee915174042e2b4b718a2f531593
Immu <shaikimam37302@gmail.com>
2026-07-01 13:24:20 +0530

    2D Array Problem Solving

---
+++ Q04/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int row,col;
+    cin>>row>>col;
+
+    int arr[row][col];
+
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<row;i++){
+        int sum=0;
+        for(int j=0;j<col;j++){
+            sum+=arr[i][j];
+        }
+        cout<<sum<<endl;
+    }
+
+    return 0;
+}
